64d8a92329fc2871d9feed9444517512c90ea38e
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-09-03 06:21:51 +0700

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,38 +2,19 @@
 import { getSortedPostsData } from '@/lib/posts';
 export const dynamic = 'force-static';
 
-export default async function generateSitemap() {
-    const siteUrl = 'https://muriabatikkudus.com';
-    const currentDate = new Date().toISOString();
+export default function sitemap() {
+    const siteUrl = 'https://sukmaaji.my.id/blogs';
 
-    // Get all blog posts
-    const posts = getSortedPostsData();
+    const posts = getSortedPostsData().map(({ slug }) => ({
+        url: `${siteUrl}/${slug}`,
+        lastModified: new Date().toISOString(),
+        changefreq: 'weekly',
+        priority: 0.7,
+    }));
 
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+    const routes = [
+        { url: siteUrl, lastModified: new Date().toISOString(), changefreq: 'weekly', priority: 1.0 },
+    ];
 
-    // Add the main website URL
-    xml += `
-  <url>
-    <loc>${siteUrl}</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>`;
-
-    // Add each post URL
-    posts.forEach(post => {
-        xml += `
-  <url>
-    <loc>${siteUrl}/${post.slug}</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>`;
-    });
-
-    xml += `
-</urlset>`;
-
-    return xml;
+    return [...routes, ...posts];
 }
